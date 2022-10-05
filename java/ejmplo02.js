45e1b86c350245fce23e648273c751c648d2d5a6
Ignacio Sauan <nachitosauanperviu@gmail.com>
2022-10-05 11:03:15 -0300

    Js clase 10

---
+++ java/ejmplo02.js
@@ -0,0 +1,9 @@
+const nombre= "Flavia"
+        var edad=40
+     
+
+        document.write( 'hola soy ' + nombre + '<br/>')
+        document.write(´hola soy ${nombre}. Mi edad es ${edad} ´)
+
+        console.log(edad)
+        console.log(´hola soy ${nombre}. mi edad es ${edad} ´)
